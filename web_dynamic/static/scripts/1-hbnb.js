94a1cd19f8ec022129610aff9ef2b7c8601f842e
david-develop <924@holbertonschool.com>
2020-02-14 14:46:34 -0500

    [Fix] semistandar in *.js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,16 @@
-$(document).ready(function(){
-  $('input[type="checkbox"]').click(function(){
-    let mydict = {}
-    if($(this).prop("checked") == true){
+const $ = window.$;
+
+$(document).ready(function () {
+  $('input[type="checkbox"]').click(function () {
+    const mydict = {};
+    if ($(this).prop('checked') === true) {
       mydict[$(this).attr('data-id')] = $(this).attr('data-name');
-    }
-    else if($(this).prop("checked") == false){
+    } else if ($(this).prop('checked') === false) {
       delete mydict[$(this).attr('data-id')];
     }
-    let mylist = []
-      for (let a in mydict){
-         mylist.push(mydict[a]);
+    const mylist = [];
+    for (const a in mydict) {
+      mylist.push(mydict[a]);
     }
     $('.amenities h4').text(mylist.join(', '));
   });
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,25 +1,26 @@
-$(document).ready(function(){
-  $('input[type="checkbox"]').click(function(){
-    let mydict = {}
-    if($(this).prop("checked") == true){
+const $ = window.$;
+
+$(document).ready(function () {
+  $('input[type="checkbox"]').click(function () {
+    const mydict = {};
+    if ($(this).prop('checked') === true) {
       mydict[$(this).attr('data-id')] = $(this).attr('data-name');
-    }
-    else if($(this).prop("checked") == false){
+    } else if ($(this).prop('checked') === false) {
       delete mydict[$(this).attr('data-id')];
     }
-    let mylist = []
-      for (let a in mydict){
-         mylist.push(mydict[a]);
+    const mylist = [];
+    for (const a in mydict) {
+      mylist.push(mydict[a]);
     }
     $('.amenities h4').text(mylist.join(', '));
   });
 
   const apiUrl = 'http://0.0.0.0:5001/api/v1/status/';
   $.get(apiUrl, function (resp) {
-    if (resp.status == 'OK') {
+    if (resp.status === 'OK') {
       $('#api_status').addClass('available');
     } else {
       $('#api_status').removeClass('available');
     }
-  })
+  });
 });
